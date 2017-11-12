9206f5512fc020021a4af95ccd0b29b7ce07a692
CelinaSkywalker <celinalandgraf@web.de>
2017-11-12 22:09:32 +0100

    finished lab4 WebService and the two extra methods

--- RestAD/src/java/restad/GenericResource.java
+++ RestAD/src/java/restad/GenericResource.java
@@ -133,7 +133,7 @@ public class GenericResource {
      * @param fecha
      * @return 
      */
-    @Path("consulta_libres")
+    @Path("reserva_plaza")
     @POST
     @Consumes("application/x-www-form-urlencoded")
     @Produces("text/html")
@@ -298,6 +298,131 @@ public class GenericResource {
             System.err.println(e.getMessage());
           }
         }   
+        return ret.toString();
+    }
+    
+    /**
+     * Web service operation
+     * @param id_hotel
+     * @param fecha
+     * @param cuantidad
+     * @return 
+     */
+    @Path("reserva_habitacion")
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/html")
+    public String reserva_varias_habitacion(@WebParam(name = "id_hotel") Integer id_hotel, @WebParam(name = "fecha") Integer fecha, @WebParam(name = "cuantidad") Integer cuantidad) {
+        Connection connection = null;
+        Integer ret = -1;
+        try                        
+        {            
+          // load the sqlite-JDBC driver using the current class loader
+            Class.forName("org.sqlite.JDBC"); 
+            connection = DriverManager.getConnection("jdbc:sqlite:/Users/Celina/Documents/00 Documentos - Celinas MacBook Air/Uni/5.Auslandssemester/AD/Ejercisios/ad-lab3/dblab3");
+            //connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Melani\\Desktop\\FIB\\TI\\AD\\lab3-ad\\dblab3");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            
+            String query = "select * from hotel_fecha where id_hotel= ? and fecha = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, id_hotel);
+            pstmt.setInt(2, fecha);
+            ResultSet rs = pstmt.executeQuery();
+            
+            while(rs.next()) {
+                if (rs.getInt("num_hab_libres") >= cuantidad) {
+                    ret = rs.getInt("num_hab_ocupadas") + cuantidad;
+                    PreparedStatement ps = connection.prepareStatement("update hotel_fecha set num_hab_ocupadas = num_hab_ocupadas +? where id_hotel= ?");
+                    ps.setInt(1, cuantidad);
+                    ps.setInt(2, id_hotel);
+                    ps.executeUpdate();
+                    PreparedStatement ps2 = connection.prepareStatement("update hotel_fecha set num_hab_libres = num_hab_libres -? where id_hotel= ?");
+                    ps2.setInt(1, cuantidad);
+                    ps2.setInt(2, id_hotel);
+                    ps2.executeUpdate();
+                }
+            }
+        }
+        catch(SQLException | ClassNotFoundException e)
+        {
+          System.err.println(e.getMessage());
+        }   
+        finally
+        {
+          try
+          {
+            if(connection != null)
+              connection.close();
+          }
+          catch(SQLException e)
+          {
+            // connection close failed.
+            System.err.println(e.getMessage());
+          }
+        }   
+        return ret.toString();
+    }
+
+    /**
+     *
+     * @param id_vuelo
+     * @param fecha
+     * @param cuantidad
+     * @return
+     */
+    @Path("reserva_varias_plaza")
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/html")
+    public String reserva_varias_plaza (@FormParam("id_vuelo") int id_vuelo, @FormParam("fecha") int fecha, @FormParam("cuantidad") int cuantidad){
+        Connection connection = null;
+        Integer ret = -1;
+        try                        
+        {            
+          // load the sqlite-JDBC driver using the current class loader
+            Class.forName("org.sqlite.JDBC"); 
+            connection = DriverManager.getConnection("jdbc:sqlite:/Users/Celina/Documents/00 Documentos - Celinas MacBook Air/Uni/5.Auslandssemester/AD/Ejercisios/ad-lab3/dblab3");
+            //connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Melani\\Desktop\\FIB\\TI\\AD\\lab3-ad\\dblab3");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            
+            String query = "select * from vuelo_fecha where id_hotel= ? and fecha = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, id_vuelo);
+            pstmt.setInt(2, fecha);
+            ResultSet rs = pstmt.executeQuery();
+            
+            while(rs.next()) {
+                if ((rs.getInt("num_plazas_max") - rs.getInt("num_plazas_ocupadas")) >= cuantidad) {
+                    ret = rs.getInt("num_plaza_ocupadas") + cuantidad;
+                    PreparedStatement ps = connection.prepareStatement("update hotel_fecha set num_plaza_ocupadas = num_plaza_ocupadas ? where id_hotel= ?");
+                    ps.setInt(1, cuantidad);
+                    ps.setInt(2, id_vuelo);
+                    ps.executeUpdate();
+                }else{
+                    ret = -1;
+                }
+            }
+        }
+        catch(SQLException | ClassNotFoundException e)
+        {
+          System.err.println(e.getMessage());
+        }   
+        finally
+        {
+          try
+          {
+            if(connection != null)
+              connection.close();
+          }
+          catch(SQLException e)
+          {
+            // connection close failed.
+            System.err.println(e.getMessage());
+          }
+        }   
+        //(ret == -1) return "<html><head/><body><h1>Error -1!</h1><br><h3>No plazas libres<h/3><body></html>";
         return ret.toString();
     }
 }
